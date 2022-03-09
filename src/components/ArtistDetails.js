9f3b9be20c9609c1c098f812300e8871ee393023
ar5137 <ar5137@student.uni-lj.si>
2022-03-09 15:58:36 +0100

    main

--- src/components/ArtistDetails.js
+++ src/components/ArtistDetails.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState, memo} from 'react';
 import axios from 'axios';
 
 
+
 export default function AllDetails() {
     
-
-
+        const [artistData, setArtistData] = useState({});
+            
         const id = '7c46c4ce-09c0-4f95-b4e1-84ae10bd24ab'
         const url = 'https://run.mocky.io/v3/'+id;
-        const [artistData, setArtistData] = useState();
+   
 
       
         
@@ -19,11 +20,13 @@ export default function AllDetails() {
                 
             })
         },[]);
+
+        //console.log(artistData.name); 
         
-       
+
      return(
         <>
-      
+            {artistData.name}
         </>
      )
  
